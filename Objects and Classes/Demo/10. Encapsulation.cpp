caca44d24c89de47d11b6588e7021717b406b7b7
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-07-02 22:18:04 +0300

    new

---
+++ Objects and Classes/Demo/10. Encapsulation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Circle {
+private:
+    double radius;
+    double area;
+public:
+    double getRadius() {
+        return this->radius;
+    }
+
+    double getArea() {
+        return this->area;
+    }
+
+    void setRadius(double radius) {
+        this->radius = radius;
+        this->area = radius * radius * PI;
+    }
+
+    Circle(double radius) :
+        radius(radius),
+        area(radius * radius * PI) {
+    }
+
+};
+
+int main() {
+    Circle c(10);
+    cout << c.getArea() << endl;
+
+    c.setRadius(20);
+    cout << c.getArea() << endl;
+
+    return 0;
+}
