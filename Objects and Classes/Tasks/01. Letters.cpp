14f59b531a7fbcfb7fa086c74195f25526f6d0de
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-07-04 01:49:24 +0300

    new

--- Objects and Classes/Tasks/01. Letters.cpp
+++ Objects and Classes/Tasks/01. Letters.cpp
@@ -1,1 +1,72 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+#include <algorithm>
 
+using namespace std;
+
+class Words {
+public:
+    typedef set<string> WordData;
+
+private:
+    WordData data;
+
+public:
+    Words (istream & ist) {
+        string buf;
+        getline(ist, buf);
+
+        for (char & c : buf) {
+            if (ispunct(c)) {
+                c = ' ';
+            }
+        }
+
+        istringstream istr(buf);
+        string tmp;
+        while (istr >> tmp) {
+            data.insert(tmp);
+        }
+    }
+
+    string getWordsWithChar(char c) {
+        ostringstream ostr;
+
+        for (const string & curr : data) {
+            string transformed = curr;
+            transform(transformed.begin(), transformed.end(), transformed.begin(), ::tolower);
+            if (transformed.find(c) != string::npos) {
+                ostr << curr << " ";
+            }
+        }
+
+        return ostr.str();
+    }
+
+    const WordData & getData(void) { return data; };
+};
+
+int main() {
+    Words words(cin);
+
+    while (true) {
+        char c;
+        cin >> c;
+
+        if (c == '.') {
+            break;
+        }
+
+        string wordsWithChar(words.getWordsWithChar(::tolower(c)));
+
+        if (wordsWithChar.length() == 0) {
+            cout << "---";
+        }
+        else {
+            cout << wordsWithChar;
+        }
+        cout << endl;
+    }
+}
