d52cf1909f336b4efd2c6bd5d67d2cedb6113c7e
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-15 23:40:27 +0300

    new

--- Objects and Classes Advanced/Tasks/01. RollingSticks/Stick.h
+++ Objects and Classes Advanced/Tasks/01. RollingSticks/Stick.h
@@ -1,2 +1,36 @@
 
 
+
+class Stick {
+private:
+    mutable char curStick;
+    mutable int position;
+
+public:
+    Stick() : curStick('/'), postiton(0) {}
+
+    char nextAnimation(void) const {
+
+        switch (curStick) {
+            case '/':
+                curStick = "_";
+                break;
+            case '_':
+                curStick = "\\";
+                break;
+            case '\\':
+                curStick = "|";
+                break;
+            case '|':
+                curStick = "/";
+                position++;
+                break;
+        }
+    }
+
+    int getPosition(void) const {
+
+        return position;
+    }
+};
+
