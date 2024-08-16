dbe9fcc21551804839ca8454f2cab258f09fbe3e
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-17 02:28:51 +0300

    new

---
+++ Objects and Classes Advanced Exercise/Tasks/05. Lectures/Resource.h
@@ -0,0 +1,67 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include "ResourceType.h"
+
+namespace SoftUni {
+    class Resource {
+    private:
+        int id;
+        ResourceType rt;
+        std::string link;
+    public:
+        int getId(void) const {
+            return id;
+        }
+
+        ResourceType getType(void) const {
+            return rt;
+        }
+
+        const std::string & getLink(void) const {
+            return link;
+        }
+
+        friend std::istream & operator >> (std::istream & istr, Resource & r);
+
+        bool operator < (const Resource & other) const {
+            return this->id < other.id;
+        }
+    };
+
+    std::istream & operator >> (std::istream & in, enum ResourceType & t) {
+        std::string buff;
+        in >> buff;
+
+        if (buff == "Demo") {
+            t = ResourceType::DEMO;
+        }
+        else if (buff == "Presentation") {
+            t = ResourceType::PRESENTATION;
+        }
+        else if (buff == "Video") {
+            t = ResourceType::VIDEO;
+        }
+
+        return in;
+    }
+
+    std::istream & operator >> (std::istream & istr, Resource & r) {
+        istr >> r.id;
+        istr >> r.rt;
+
+        istr.ignore();
+        getline(istr, r.link);
+
+        return istr;
+    }
+
+    std::ostream & operator << (std::ostream & out, const Resource & r) {
+        out << r.getId() << ' ' << r.getType() << ' ' << r.getLink();
+
+        return out;
+    }
+}
+
+#endif
+
