5bf1fc7f590571eaa353dfafac47e5ca6cb77d64
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-17 02:55:36 +0300

    new

--- Objects and Classes Advanced Exercise/Tasks/05. Lectures/Lecture.h
+++ Objects and Classes Advanced Exercise/Tasks/05. Lectures/Lecture.h
@@ -1,6 +1,60 @@
 #ifndef LECTURE_H
 #define LECTURE_H
 
+#include <set>
+#include "Resource.h"
 
+namespace SoftUni {
+    typedef std::vector<ResourceType> ResourceVector;
+
+    class Lecture {
+        public:
+            typedef std::set<Resource> ResourceSet;
+            typedef std::map<ResourceType, int> ResourcesCount;
+
+        private:
+            ResourceSet resources;
+            ResourcesCount count;
+
+        public:
+            ResourceSet::iterator begin() {
+                return resources.begin();
+            }
+            ResourceSet::iterator end() {
+                return resources.end();
+            }
+
+            Lecture & operator << (const Resource & r) {
+                ResourceSet::iterator itFound = resources.find(r);
+                if (itFound != resources.end()) {
+                    ResourceType rt = r.getType();
+                    resources.erase(itFound);
+                    count[rt]--;
+
+                    if (count[rt] == 0) {
+                        count.erase(rt);
+                    }
+                }
+                resources.insert(r);
+                count[r.getType()]++;
+
+                return *this;
+            }
+
+            int operator [] (ResourceType rt) {
+                return count[rt];
+            }
+
+        friend ResourceVector & operator << (ResourceVector & vec, Lecture & l);
+    };
+
+    ResourceVector & operator << (ResourceVector & vec, Lecture & l) {
+        for (auto curr : l.count) {
+            vec.push_back(curr.first);
+        }
+
+        return vec;
+    }
+}
 
 #endif
