5f5c7d9c406dad0797aac9661ce19eb159d0f7a7
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-16 03:52:56 +0300

    new

--- Objects and Classes Advanced Exercise/Tasks/03. Operators/Operators.h
+++ Objects and Classes Advanced Exercise/Tasks/03. Operators/Operators.h
@@ -1,9 +1,29 @@
 #ifndef OPERATORS_H
 #define OPERATORS_H
 
+#include <ostream>
 #include <vector>
 #include <string>
-#include <iostream>
+#include <sstream>
+
+std::vector<std::string> & operator << (std::vector<std::string> & vectorString, const std::string & lineToAdd) {
+    vectorString.push_back(lineToAdd);
+    return vectorString;
+}
+
+std::string operator + (const std::string & str, int i) {
+    std::ostringstream ostr;
+    ostr << str << i;
+
+    return ostr.str();
+}
+
+std::ostream & operator << (std::ostream & ostr, const std::vector<std::string> & vect) {
+    for (const std::string & curr : vect) {
+        ostr << curr << std::endl;
+    }
+    return ostr;
+}
 
 
 #endif
