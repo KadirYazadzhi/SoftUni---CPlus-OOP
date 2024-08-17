3e9bf067d0f3e0a77300562efb27fb8e42496584
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-17 22:40:14 +0300

    new

--- Code Organization and Templates/Demo/01. Include And Define/IncludeAndDefineMain.cpp
+++ Code Organization and Templates/Demo/01. Include And Define/IncludeAndDefineMain.cpp
@@ -1,18 +1,19 @@
-#include <iostream> // directly looks for system file iostream
-#include "Macros.h" // first looks for local file "01. Macros.h" - if not found, searches for system file
+#include <iostream>
+#include "Macros.h"
+
+using namespace std;
 
 int main() {
-	double radius;
-	std::cin >> radius;
+    double radius;
+    cin >> radius;
 
-	std::cout << PI << std::endl;
+    cout << PI << endl;
 
-	SHOW(PI * radius * radius);
+    SHOW(PI * radius * radius);
 #undef SHOW
-#define SHOW std::cout << "That's all, folks!" << std::endl;
-	SHOW
+#define SHOW cout << "That's all, folks!" << endl;
+    SHOW
 #undef SHOW
-	// The following line won't compile because SHOW is no longer defined
-	// SHOW
-	return 0;
+
+return 0;
 }
