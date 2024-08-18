295273c9d266f17ec5092d24f06268d0034d56d8
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-18 05:08:44 +0300

    new

--- Code Organization and Templates/Tasks/04. Split And Join/Join.h
+++ Code Organization and Templates/Tasks/04. Split And Join/Join.h
@@ -1,4 +1,20 @@
 #ifndef JOIN_H
 #define JOIN_H
 
+#include <string>
+#include <sstream>
+#include <vector>
+#include <ostream>
+
+template<class T>
+std::string join(const std::vector<T> & v, const std::string & joinStr) {
+    std::ostringstream ostr;
+
+    for (const T & curr : v) {
+        ostr << curr << joinStr;
+    }
+
+    return ostr.str();
+}
+
 #endif
--- Code Organization and Templates/Tasks/04. Split And Join/Split.h
+++ Code Organization and Templates/Tasks/04. Split And Join/Split.h
@@ -1,4 +1,29 @@
 #ifndef SPLIT_H
 #define SPLIT_H
 
+#include <string>
+#include <vector>
+#include <sstream>
+#include <istream>
+
+template<class T>
+std::vector<T> split(const std::string & line, char separator) {
+    std::istringstream is(line);
+    std::vector<T> res;
+
+    while (true) {
+        std::string buffer;
+        if (!getline(is, buffer, separator)) {
+            break;
+        }
+
+        std::istringstream eltS(buffer);
+        T t;
+        eltS >> t;
+        res.push_back(t);
+    }
+
+    return res;
+}
+
 #endif
