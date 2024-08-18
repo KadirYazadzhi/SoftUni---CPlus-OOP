29242f930a45580e28e468433bb00d38c5341c0c
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-18 05:13:20 +0300

    new

--- Code Organization and Templates/Tasks/04. Split And Join/Join.h
+++ Code Organization and Templates/Tasks/04. Split And Join/Join.h
@@ -10,8 +10,15 @@ template<class T>
 std::string join(const std::vector<T> & v, const std::string & joinStr) {
     std::ostringstream ostr;
 
+    bool bFirst = true;
     for (const T & curr : v) {
-        ostr << curr << joinStr;
+        if (!bFirst) {
+            ostr << joinStr;
+        }
+        else {
+            bFirst = false;
+        }
+        ostr << curr;
     }
 
     return ostr.str();
