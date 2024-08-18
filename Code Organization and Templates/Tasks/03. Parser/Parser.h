d10b95cb90f01cee95a3e820ec579229ab743335
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-18 03:59:25 +0300

    new

--- Code Organization and Templates/Tasks/03. Parser/Parser.h
+++ Code Organization and Templates/Tasks/03. Parser/Parser.h
@@ -1,6 +1,29 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <istream>
+#include <string>
+#include <sstream>
 
+template <typename T>
+class Parser {
+    private:
+        std::istream & is;
+        std::string stopLine;
+    public:
+        Parser(std::istream & is, std::string stopLine) : is(is), stopLine(stopLine) {}
+
+        bool readNext(T & t) {
+            std::string line;
+            getline(is, line);
+            if (line == stopLine) {
+                return false;
+            }
+            std::istringstream istr(line);
+            istr >> t;
+
+            return true;
+        }
+};
 
 #endif
--- Code Organization and Templates/Tasks/03. Parser/PrintUtils.h
+++ Code Organization and Templates/Tasks/03. Parser/PrintUtils.h
@@ -1,4 +1,14 @@
 #ifndef PRINTUTILS_H
 #define PRINTUTILS_H
 
+template <typename T>
+printVector(T & v) {
+    typename T::iterator itC = v.begin();
+
+    for (; itC != v.end; itC++) {
+        std::cout << *itC << ' ';
+    }
+    std::cout << std::endl;
+}
+
 #endif
