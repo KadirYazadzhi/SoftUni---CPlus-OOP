287403cee3603312c1f9eeb79e7d71a3fd907f4d
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-19 03:58:20 +0300

    new

---
+++ Exercise - OOP Constructors/03. Order/OrderedInserter.h
@@ -0,0 +1,27 @@
+#ifndef ORDEREDINSERTER_H
+#define ORDEREDINSERTER_H
+
+#include "Company.h"
+#include <vector>
+#include <algorithm>
+
+class OrderedInserter {
+    private:
+        typedef std::vector<const Company*> companyVector;
+        companyVector companies;
+
+    public:
+        OrderedInserter(companyVector & companies) : companies(companies) {
+            void insert(const Company * c) {
+                companyVector::iterator it = find_if(companies.begin(),
+                                                     companies.end(),
+                                                     [c] (const Company *curr)) {
+                    return curr->getId() > c.getId();
+                };
+
+                companies.insert(it, c);
+            }
+        }
+};
+
+#endif
