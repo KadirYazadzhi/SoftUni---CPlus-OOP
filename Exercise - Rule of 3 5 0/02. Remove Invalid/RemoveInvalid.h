ec0c52f4fb44e3f2129c872bb34905cadc24fdaa
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-20 05:10:02 +0300

    new

--- Exercise - Rule of 3 5 0/02. Remove Invalid/RemoveInvalid.h
+++ Exercise - Rule of 3 5 0/02. Remove Invalid/RemoveInvalid.h
@@ -1,6 +1,21 @@
 #ifndef REMOVEINVALID_H
 #define REMOVEINVALID_H
 
+#include "Company.h"
+#include <list>
 
+void removeInvalid(std::list<Company*> & companies) {
+    std::list<Company*>::iterator it = companies.begin();
+
+    for (;it != companies.end(); ) {
+        if ((*it)->getId() < 0) {
+            delete * it;
+            it = companies.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+}
 
 #endif
