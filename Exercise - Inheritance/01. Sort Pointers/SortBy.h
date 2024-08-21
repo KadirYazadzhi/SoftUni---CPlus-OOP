f6cba3d2d01aeb91d3d842e1d7cca776ffe3b4e0
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-21 08:07:16 +0300

    new

--- Exercise - Inheritance/01. Sort Pointers/SortBy.h
+++ Exercise - Inheritance/01. Sort Pointers/SortBy.h
@@ -4,18 +4,19 @@
 #include "Company.h"
 #include <algorithm>
 
-void sortBy(Company ** start, Company ** end, bool(*compare)(const Company & a, const Company & b)) {
+void sortBy(Company** start, Company** end, bool(*compare)(const Company& a, const Company& b)) {
     struct SortFunc {
-        bool(*compare)(const Company & a, const Company & b);
+        bool(*compare)(const Company& a, const Company& b);
 
-        SortFunc(bool(*compare)(const Company & a, Company & b)) : compare(compare) {}
+        SortFunc(bool(*compare)(const Company& a, const Company& b)) : compare(compare) {}
 
-        bool operator() (Company * first, Company * second) {
+        bool operator() (const Company* first, const Company* second) const {
             return compare(*first, *second);
         }
     };
 
-    std::sort(start, end, SortFunc(compare);)
+    std::sort(start, end, SortFunc(compare));
 }
 
 #endif
+
