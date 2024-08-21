0c0a84d2f591cb9dccf75a440a5460194296d510
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-21 06:55:48 +0300

    new

--- Exercise - Inheritance/03. MaxSumArray/Array.h
+++ Exercise - Inheritance/03. MaxSumArray/Array.h
@@ -1,6 +1,15 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <vector>
 
+template<typename T> class Array : public std::vector<T> {
+public:
+    Array (size_t size = 0) : std::vector<T>(size) {}
+
+    size_t getSize(void) const {
+        return this->size();
+    }
+};
 
 #endif
