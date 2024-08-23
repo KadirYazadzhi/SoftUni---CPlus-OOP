bc8b74ea4669ece991a94ce929296126853694c0
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-23 06:31:46 +0300

    new

--- Exercise - Abstract Classes/Tasks/03. Files/Directory.h
+++ Exercise - Abstract Classes/Tasks/03. Files/Directory.h
@@ -1,4 +1,33 @@
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
+#include "FileSystemObjectsContainer.h"
+
+#include <list>
+
+class Directory: public FileSystemObject, public FileSystemObjectsContainer {
+
+    std::list<std::shared_ptr<FileSystemObject>> contents;
+
+public:
+    Directory(std::string name) : FileSystemObject(name) {};
+
+
+    virtual size_t getSize() const {
+
+        size_t size = 0;
+
+        for (std::shared_ptr<FileSystemObject> curr : contents) {
+            size+=curr->getSize();
+        };
+        return size;
+    };
+
+
+    virtual void add(const std::shared_ptr<FileSystemObject>& obj) {
+        contents.push_back(obj);
+    };
+
+};
+
 #endif
